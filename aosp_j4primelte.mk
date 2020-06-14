14c486659121d46893363664f539ca8bcfc54748
Joey Huab <joey@evolution-x.org>
2020-06-14 20:42:23 -0300

    j4primelte: Update to Coral's June fingerprint

--- aosp_j4primelte.mk
+++ aosp_j4primelte.mk
@@ -40,4 +40,7 @@ TARGET_VENDOR_PRODUCT_NAME := j4primelte
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j4primeltedx-user 9 PPR1.180610.011 J415FXXU2BSDM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "samsung/j4primeltedx/j4primelte:9/PPR1.180610.011/J415FXXU2BSDM:user/release-keys"
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys
